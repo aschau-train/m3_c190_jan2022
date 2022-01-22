9b7fb9f22d0558deabe16132706cbae3f3912265
Augie Schau <aschau@wiley.com>
2022-01-22 12:47:46 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/ajs/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author augie
+ * email: aschau@wiley.com
+ * date: January 22, 2022
+ * purpose: My first Hello World program in an IDE
+ */
+package com.mthree.ajs.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Augie!");
+    }
+            
+}
