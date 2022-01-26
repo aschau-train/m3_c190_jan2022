8cc2d218dd5ea9dcb5b77236f61581e71ad96aa2
Augie Schau <aschau@wiley.com>
2022-01-26 14:53:49 -0500

    Rearranging projects and adding WindowMaster

---
+++ DemoTwo/src/main/java/com/sg/foundations/basics/HelloIDE.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author augie
+ *email: aschau@wiley.com
+ *date: 2022.01.25
+ *purpose:
+ */
+public class HelloIDE {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from NetBeans");
+        
+    }
+}
